7233baac791adbb3535ead0ba2015a2f3312c81d
Lucas Ribeiro <lucasribeiroalves@github.no-reply.com>
2026-07-22 05:07:31 -0300

    Revert "correcoes de build"
    This reverts commit 0f535c37271a4c305981cc4c17e1c63894338f6c.

--- app/ext/migration/__init__.py
+++ app/ext/migration/__init__.py
@@ -1,14 +1,7 @@
-try:
-    from flask_migrate import Migrate
-except ImportError:
-    Migrate = None
-
+from flask_migrate import Migrate
 from app.ext.database import db
 from app.ext.database.models import *
 
 
 def init_app(app):
-    if Migrate is None:
-        return
-
-    Migrate(app, db)
+    migrate = Migrate(app, db)
