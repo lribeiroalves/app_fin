0f535c37271a4c305981cc4c17e1c63894338f6c
Lucas Ribeiro <lucasribeiroalves@github.no-reply.com>
2026-07-22 04:34:52 -0300

    correcoes de build

--- app/ext/migration/__init__.py
+++ app/ext/migration/__init__.py
@@ -1,7 +1,14 @@
-from flask_migrate import Migrate
+try:
+    from flask_migrate import Migrate
+except ImportError:
+    Migrate = None
+
 from app.ext.database import db
 from app.ext.database.models import *
 
 
 def init_app(app):
-    migrate = Migrate(app, db)
+    if Migrate is None:
+        return
+
+    Migrate(app, db)
