5c02134f6f21a045bb64634dfa5e5107917051c0
Lucas Ribeiro <lucasribeiroalves@github.no-reply.com>
2026-07-23 03:39:41 -0300

    Criada a tabela Transacoes e sua migration

--- app/ext/database/models.py
+++ app/ext/database/models.py
@@ -1,7 +1,8 @@
 from . import db
 
-from sqlalchemy import String
+from sqlalchemy import String, Numeric, ForeignKey, Integer, CheckConstraint, Enum as SQLEnum
 from sqlalchemy.orm import Mapped, mapped_column, relationship
+from enum import Enum
 from typing import Optional
 from datetime import datetime
 
@@ -32,3 +33,26 @@ class Bancos(db.Model):
             'id': self.id,
             'nome': self.nome
         }
+
+
+class TipoTransacao(str, Enum):
+    ENTRADA = 'ENTRADA'
+    SAIDA = 'SAIDA'
+
+
+class Transacoes(db.Model):
+    __table_args__ = (
+        CheckConstraint("ano >= 2026", name="check_ano_valido"),
+        CheckConstraint("mes >= 1 AND mes <= 12", name="check_mes_valido"),
+    )
+
+    id: Mapped[int] = mapped_column(primary_key=True)
+    tipo: Mapped[TipoTransacao] = mapped_column(SQLEnum(TipoTransacao, native_enum=False), nullable=False)
+    ano: Mapped[int] = mapped_column(Integer, nullable=False)
+    mes: Mapped[int] = mapped_column(Integer, nullable=False)
+    descricao: Mapped[str] = mapped_column(String(255), nullable=False)
+    valor: Mapped[float] = mapped_column(Numeric(precision=10, scale=2), nullable=False)
+
+    # Relacoes
+    user_id: Mapped[int] = mapped_column(ForeignKey('users.id'))
+    user: Mapped['Users'] = relationship('Users')
