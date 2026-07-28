e8b4df121996aaec8b35dd3fb6d8677cd9d16728
Lucas Ribeiro <lucasribeiroalves@github.no-reply.com>
2026-07-28 05:15:16 -0300

    Ajuste de tamanho de tela da aplicacao

--- main.py
+++ main.py
@@ -5,6 +5,6 @@ from app import create_app
 if __name__ == '__main__':
     app = create_app()
 
-    window = webview.create_window('App Financeiro', app, width=1024, height=768)
+    window = webview.create_window('App Financeiro', app, width=5000, height=5000, resizable=False)
 
     webview.start(http_server=True)
