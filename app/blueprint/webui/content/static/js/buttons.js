c721dd027574de3a173a4e59b1d78b5c52eeb584
Lucas Ribeiro <lucasribeiroalves@github.no-reply.com>
2026-08-03 04:06:13 -0300

    remocao de arquivos desnecessarios

--- app/blueprint/webui/content/static/js/buttons.js
+++
@@ -1,6 +0,0 @@
-btn_nova_entrada = document.getElementById('btn-nova-entrada');
-
-function abrirModalEntrada() {
-    const modalEntrada = bootstrap.Modal.getOrCreateInstance(document.getElementById('modalEntrada'));
-    modalEntrada.show();
-}
