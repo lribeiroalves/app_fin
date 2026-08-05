fc7ed4961f67f5d02fe782ed260a964f4d4e5db0
Lucas Ribeiro <lucasribeiroalves@github.no-reply.com>
2026-08-04 22:58:48 -0300

    separacao das views por formulario ok

--- app/blueprint/webui/__init__.py
+++ app/blueprint/webui/__init__.py
@@ -1,5 +1,5 @@
 from flask import Blueprint
-from .views import index, users, bancos
+from .views import index, users, bancos, form_filtro, form_transaction, form_saldo, form_banco
 from app import get_base_path
 import os
 from datetime import datetime
@@ -11,6 +11,10 @@ bp = Blueprint('webui', __name__, static_folder=os.path.join(base_path, 'app', '
 
 # URLs
 bp.add_url_rule('/', view_func=index, methods=['GET', 'POST'])
+bp.add_url_rule('/form-filtro', view_func=form_filtro, methods=['POST'])
+bp.add_url_rule('/form-transaction', view_func=form_transaction, methods=['POST'])
+bp.add_url_rule('/form-saldo', view_func=form_saldo, methods=['POST'])
+bp.add_url_rule('/form-banco', view_func=form_banco, methods=['POST'])
 bp.add_url_rule('/users', view_func=users)
 bp.add_url_rule('/bancos', view_func=bancos)
 
